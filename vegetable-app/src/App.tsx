7de36a17ec883086c136e65fc171991a9980d171
Ubuntu <ajitsingh@ubuntudevajit.45cpkteljfdejhp55hm3wujoce.frax.internal.cloudapp.net>
2025-01-04 09:17:38 +0000

    react app created

---
+++ vegetable-app/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
