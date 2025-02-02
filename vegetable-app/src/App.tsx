4e4f43cfb9735756b76f3c69401d0e580cc3a1d4
Ajit Singh Shekhawat <ajit@sankhyatronics.com>
2025-02-02 09:38:57 +0100

    header in progress

--- vegetable-app/src/App.tsx
+++ vegetable-app/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import './App.css';
+import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
 import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route index path='Home' element={<Home />} />
+          <Route index path='AboutUs' element={<AboutUs />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ vegetable-app/src/pages/AboutUs.tsx
@@ -0,0 +1,6 @@
+function AboutUs() {
+    return (<div>About us </div>);
+  }
+  
+  export default AboutUs;
+  
