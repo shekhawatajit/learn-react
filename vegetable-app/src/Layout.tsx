11e7857b0e013dd219fc79bde87bb4e3eca34ee0
rocky-bana <shekhawatrudra@hotmail.com>
2025-02-02 16:05:08 +0530

    Footer WIP

--- vegetable-app/src/Layout.tsx
+++ vegetable-app/src/Layout.tsx
@@ -1,17 +1,22 @@
 import { Outlet } from "react-router-dom";
 import TopBar from "./components/TopBar";
 import NavBar from "./components/NavBar";
+import './index.css';
+import NavFooter from "./components/NavFooter";
 
 function Layout() {
   return (<div className="flex flex-col mx-auto mx-16 p-8...">
+    <header>
       <TopBar />
       <NavBar />
+    </header>
     <div className="flex-col">
-    <Outlet />
+      <Outlet />
     </div>
-    <div className="flex-col">
-      
-    </div>
+    <footer className="flex-col text-white min-h-32 bg-dark">
+    <NavFooter/>
+    <hr className="bg-secondary"/>
+    </footer>
   </div>
   );
 }
--- vegetable-app/tailwind.config.js
+++ vegetable-app/tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
         './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          primary: '#81C408',
+          secondary: '#FFB524',
+          dark: '#45595B'
+        },
+      },
     },
     plugins: [],
-  }
+   }
